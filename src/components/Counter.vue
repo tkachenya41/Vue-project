bbf36cf6376073e935d439e5a8202b25a3df8d83
tkachenya41 <tkachenya41@gmail.com>
2024-01-31 16:05:08 +0300

    feat: add counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="counter">
+    <h3>Counter {{ count }}</h3>
+    <button @click="increase">Increase</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Counter",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    },
+  },
+});
+</script>
+
+<style>
+.counter {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+</style>
